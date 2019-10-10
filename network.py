9e5d6ac6813ff7868e46dc490df4025a11a5fcd5
lixiaolei1982 <44882833+lixiaolei1982@users.noreply.github.com>
2019-10-10 21:42:47 +0800

    Add files via upload

---
+++ network.py
@@ -0,0 +1,274 @@
+
+
+from keras.layers import Conv2D, MaxPooling2D, UpSampling2D, BatchNormalization, Reshape, Permute, Activation, Input, \
+    add, multiply
+from keras.layers import concatenate, core, Dropout
+from keras.models import Model
+from keras.layers.merge import concatenate
+from keras.optimizers import Adam
+from keras.optimizers import SGD
+from keras.layers.core import Lambda
+import keras.backend as K
+
+
+
+
+def up_and_concate(down_layer, layer, data_format='channels_first'):
+    if data_format == 'channels_first':
+        in_channel = down_layer.get_shape().as_list()[1]
+    else:
+        in_channel = down_layer.get_shape().as_list()[3]
+
+    # up = Conv2DTranspose(out_channel, [2, 2], strides=[2, 2])(down_layer)
+    up = UpSampling2D(size=(2, 2), data_format=data_format)(down_layer)
+
+    if data_format == 'channels_first':
+        my_concat = Lambda(lambda x: K.concatenate([x[0], x[1]], axis=1))
+    else:
+        my_concat = Lambda(lambda x: K.concatenate([x[0], x[1]], axis=3))
+
+    concate = my_concat([up, layer])
+
+    return concate
+
+
+def attention_up_and_concate(down_layer, layer, data_format='channels_first'):
+    if data_format == 'channels_first':
+        in_channel = down_layer.get_shape().as_list()[1]
+    else:
+        in_channel = down_layer.get_shape().as_list()[3]
+
+    # up = Conv2DTranspose(out_channel, [2, 2], strides=[2, 2])(down_layer)
+    up = UpSampling2D(size=(2, 2), data_format=data_format)(down_layer)
+
+    layer = attention_block_2d(x=layer, g=up, inter_channel=in_channel // 4, data_format=data_format)
+
+    if data_format == 'channels_first':
+        my_concat = Lambda(lambda x: K.concatenate([x[0], x[1]], axis=1))
+    else:
+        my_concat = Lambda(lambda x: K.concatenate([x[0], x[1]], axis=3))
+
+    concate = my_concat([up, layer])
+    return concate
+
+
+def attention_block_2d(x, g, inter_channel, data_format='channels_first'):
+    # theta_x(?,g_height,g_width,inter_channel)
+
+    theta_x = Conv2D(inter_channel, [1, 1], strides=[1, 1], data_format=data_format)(x)
+
+    # phi_g(?,g_height,g_width,inter_channel)
+
+    phi_g = Conv2D(inter_channel, [1, 1], strides=[1, 1], data_format=data_format)(g)
+
+    # f(?,g_height,g_width,inter_channel)
+
+    f = Activation('relu')(add([theta_x, phi_g]))
+
+    # psi_f(?,g_height,g_width,1)
+
+    psi_f = Conv2D(1, [1, 1], strides=[1, 1], data_format=data_format)(f)
+
+    rate = Activation('sigmoid')(psi_f)
+
+    # rate(?,x_height,x_width)
+
+    # att_x(?,x_height,x_width,x_channel)
+
+    att_x = multiply([x, rate])
+
+    return att_x
+
+
+def res_block(input_layer, out_n_filters, batch_normalization=False, kernel_size=[3, 3], stride=[1, 1],
+
+              padding='same', data_format='channels_first'):
+    if data_format == 'channels_first':
+        input_n_filters = input_layer.get_shape().as_list()[1]
+    else:
+        input_n_filters = input_layer.get_shape().as_list()[3]
+
+    layer = input_layer
+    for i in range(2):
+        layer = Conv2D(out_n_filters // 4, [1, 1], strides=stride, padding=padding, data_format=data_format)(layer)
+        if batch_normalization:
+            layer = BatchNormalization()(layer)
+        layer = Activation('relu')(layer)
+        layer = Conv2D(out_n_filters // 4, kernel_size, strides=stride, padding=padding, data_format=data_format)(layer)
+        layer = Conv2D(out_n_filters, [1, 1], strides=stride, padding=padding, data_format=data_format)(layer)
+
+    if out_n_filters != input_n_filters:
+        skip_layer = Conv2D(out_n_filters, [1, 1], strides=stride, padding=padding, data_format=data_format)(
+            input_layer)
+    else:
+        skip_layer = input_layer
+    out_layer = add([layer, skip_layer])
+    return out_layer
+
+
+# Recurrent Residual Convolutional Neural Network based on U-Net (R2U-Net)
+def rec_res_block(input_layer, out_n_filters, batch_normalization=False, kernel_size=[3, 3], stride=[1, 1],
+
+                  padding='same', data_format='channels_first'):
+    if data_format == 'channels_first':
+        input_n_filters = input_layer.get_shape().as_list()[1]
+    else:
+        input_n_filters = input_layer.get_shape().as_list()[3]
+
+    if out_n_filters != input_n_filters:
+        skip_layer = Conv2D(out_n_filters, [1, 1], strides=stride, padding=padding, data_format=data_format)(
+            input_layer)
+    else:
+        skip_layer = input_layer
+
+    layer = skip_layer
+    for j in range(2):
+
+        for i in range(2):
+            if i == 0:
+
+                layer1 = Conv2D(out_n_filters, kernel_size, strides=stride, padding=padding, data_format=data_format)(
+                    layer)
+                if batch_normalization:
+                    layer1 = BatchNormalization()(layer1)
+                layer1 = Activation('relu')(layer1)
+            layer1 = Conv2D(out_n_filters, kernel_size, strides=stride, padding=padding, data_format=data_format)(
+                add([layer1, layer]))
+            if batch_normalization:
+                layer1 = BatchNormalization()(layer1)
+            layer1 = Activation('relu')(layer1)
+        layer = layer1
+
+    out_layer = add([layer, skip_layer])
+    return out_layer
+
+########################################################################################################
+# Define the neural network
+def unet(img_w, img_h, n_label, data_format='channels_first'):
+    inputs = Input((3, img_w, img_h))
+    x = inputs
+    depth = 4
+    features = 64
+    skips = []
+    for i in range(depth):
+        x = Conv2D(features, (3, 3), activation='relu', padding='same', data_format=data_format)(x)
+        x = Dropout(0.2)(x)
+        x = Conv2D(features, (3, 3), activation='relu', padding='same', data_format=data_format)(x)
+        skips.append(x)
+        x = MaxPooling2D((2, 2), data_format= data_format)(x)
+        features = features * 2
+
+    x = Conv2D(features, (3, 3), activation='relu', padding='same', data_format=data_format)(x)
+    x = Dropout(0.2)(x)
+    x = Conv2D(features, (3, 3), activation='relu', padding='same', data_format=data_format)(x)
+
+    for i in reversed(range(depth)):
+        features = features // 2
+        # attention_up_and_concate(x,[skips[i])
+        x = UpSampling2D(size=(2, 2), data_format=data_format)(x)
+        x = concatenate([skips[i], x], axis=1)
+        x = Conv2D(features, (3, 3), activation='relu', padding='same', data_format=data_format)(x)
+        x = Dropout(0.2)(x)
+        x = Conv2D(features, (3, 3), activation='relu', padding='same', data_format=data_format)(x)
+
+    conv6 = Conv2D(n_label, (1, 1), padding='same', data_format=data_format)(x)
+    conv7 = core.Activation('sigmoid')(conv6)
+    model = Model(inputs=inputs, outputs=conv7)
+
+    #model.compile(optimizer=Adam(lr=1e-5), loss=[focal_loss()], metrics=['accuracy', dice_coef])
+    return model
+
+
+########################################################################################################
+#Attention U-Net
+def att_unet(img_w, img_h, n_label, data_format='channels_first'):
+    inputs = Input((3, img_w, img_h))
+    x = inputs
+    depth = 4
+    features = 64
+    skips = []
+    for i in range(depth):
+        x = Conv2D(features, (3, 3), activation='relu', padding='same', data_format=data_format)(x)
+        x = Dropout(0.2)(x)
+        x = Conv2D(features, (3, 3), activation='relu', padding='same', data_format=data_format)(x)
+        skips.append(x)
+        x = MaxPooling2D((2, 2), data_format='channels_first')(x)
+        features = features * 2
+
+    x = Conv2D(features, (3, 3), activation='relu', padding='same', data_format=data_format)(x)
+    x = Dropout(0.2)(x)
+    x = Conv2D(features, (3, 3), activation='relu', padding='same', data_format=data_format)(x)
+
+    for i in reversed(range(depth)):
+        features = features // 2
+        x = attention_up_and_concate(x, skips[i], data_format=data_format)
+        x = Conv2D(features, (3, 3), activation='relu', padding='same', data_format=data_format)(x)
+        x = Dropout(0.2)(x)
+        x = Conv2D(features, (3, 3), activation='relu', padding='same', data_format=data_format)(x)
+
+    conv6 = Conv2D(n_label, (1, 1), padding='same', data_format=data_format)(x)
+    conv7 = core.Activation('sigmoid')(conv6)
+    model = Model(inputs=inputs, outputs=conv7)
+
+    #model.compile(optimizer=Adam(lr=1e-5), loss=[focal_loss()], metrics=['accuracy', dice_coef])
+    return model
+
+
+########################################################################################################
+#Recurrent Residual Convolutional Neural Network based on U-Net (R2U-Net)
+def r2_unet(img_w, img_h, n_label, data_format='channels_first'):
+    inputs = Input((3, img_w, img_h))
+    x = inputs
+    depth = 4
+    features = 64
+    skips = []
+    for i in range(depth):
+        x = rec_res_block(x, features, data_format=data_format)
+        skips.append(x)
+        x = MaxPooling2D((2, 2), data_format=data_format)(x)
+
+        features = features * 2
+
+    x = rec_res_block(x, features, data_format=data_format)
+
+    for i in reversed(range(depth)):
+        features = features // 2
+        x = up_and_concate(x, skips[i], data_format=data_format)
+        x = rec_res_block(x, features, data_format=data_format)
+
+    conv6 = Conv2D(n_label, (1, 1), padding='same', data_format=data_format)(x)
+    conv7 = core.Activation('sigmoid')(conv6)
+    model = Model(inputs=inputs, outputs=conv7)
+    #model.compile(optimizer=Adam(lr=1e-6), loss=[dice_coef_loss], metrics=['accuracy', dice_coef])
+    return model
+
+
+########################################################################################################
+#Attention R2U-Net
+def att_r2_unet(img_w, img_h, n_label, data_format='channels_first'):
+    inputs = Input((3, img_w, img_h))
+    x = inputs
+    depth = 4
+    features = 64
+    skips = []
+    for i in range(depth):
+        x = rec_res_block(x, features, data_format=data_format)
+        skips.append(x)
+        x = MaxPooling2D((2, 2), data_format=data_format)(x)
+
+        features = features * 2
+
+    x = rec_res_block(x, features, data_format=data_format)
+
+    for i in reversed(range(depth)):
+        features = features // 2
+        x = attention_up_and_concate(x, skips[i], data_format=data_format)
+        x = rec_res_block(x, features, data_format=data_format)
+
+    conv6 = Conv2D(n_label, (1, 1), padding='same', data_format=data_format)(x)
+    conv7 = core.Activation('sigmoid')(conv6)
+    model = Model(inputs=inputs, outputs=conv7)
+    #model.compile(optimizer=Adam(lr=1e-6), loss=[dice_coef_loss], metrics=['accuracy', dice_coef])
+    return model
+
+
